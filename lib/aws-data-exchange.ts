8f98c8595fda8b74b04e7385421c905f3a241934
zivnadel <zywn1414@gmail.com>
2022-11-13 00:14:50 +0200

    Fixed environment variables for deployment

--- lib/aws-data-exchange.ts
+++ lib/aws-data-exchange.ts
@@ -14,8 +14,8 @@ const logger = {
 };
 
 const credentials = {
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  accessKeyId: process.env.ACCESS_KEY_ID!,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY!,
 };
 
 // instantiate the DataExchangeClient
